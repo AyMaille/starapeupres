883ac9666a8c2d94b33d4d9033097715386a71cd
florian olhasque <olhasque.florian@gmail.com>
2021-08-17 15:16:50 +0200

    controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :stars
+  # resources :users do
+  #   resources :stars
+  #   resources :missions
+  # end
+  resources :missions
 end
